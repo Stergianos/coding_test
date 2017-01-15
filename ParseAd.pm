0d6f6e46a3c0c23e4c14caf7b88208a38c72bbba
Stergianos <stergianosfm@gmail.com>
2017-01-16 00:58:48 +0200

    New Version

--- ParseAd.pm
+++ ParseAd.pm
@@ -1,91 +1,86 @@
 package ParseAd;
-use Data::Dumper;
+use Data::Dumper qw(Dumper);
 use strict;
 use warnings;
 
-sub ad_fields {
 
-            my($paramName) = @_; #pass parameter
-            my %result;
-
-            my $filename = "data/$paramName.html";
-            
-              open(my $fh, "<:encoding(UTF-8)", $filename)   #open file
-                || die "Can't open UTF-8 encoded $filename: $!";
-                
-               while (my $title = $fh) {
-
-                    if ( $title =~ /title">(.*?)h1>/) {
-
-                        print $title;
-                        %result=(title => $title,
-                                location => '',
-                                sector => '',
-                                description => '',
-                                id => ''
-                        );
-
-                    }
-                
-                    my $location = '/jobLocation">(.*?)<b>/';
-                   if ( $title =~ $location) {
-                        print $location;
-                        %result=(
-                                location => $title,
-                                sector => '',
-                                id => ''
-                        );
-
-                    }
-                    
-
-
-                        my $sector = '/Sector:(.*?)Medical/';
-                    if (  $title =~ $sector ) {
-                        print $sector;
-                        %result=(
-                                sector => $title,
-                                id => ''
-                        );
-
-                    }
-                        my $description='/description">(.*?)beneficial/';
-                     if (  $title =~ $description ) {
-                        print $description;
-                        %result=(
-                                decription => $title,
-                                id => ''
-                        );
-
-                    }
-                        my $id = '/(.*?)/';
-                    if (  $title =~ $id) { #i cant find the id in the ad
-                        print $id;
-                        %result=(
-
-                                id => $title
-                        );
-                      
-                    }
-
-
-
-            }
-
-                  
-         return %result;         
-    } #end of sub
+# NOTE: There is no need to add anyting outside of this function
+sub ad_fields
+{
+    my %result=(title => '',
+                location=>'',
+                sector=>'',
+                description=>'',
+                id=>''
+         );
+         my @results;
+	my $filename = shift;
+    open(my $fh, "<:encoding(UTF-8)", $filename)   #open file
+     || die "Can't open UTF-8 encoded $filename: $!";
+     
+    while (my $line = <$fh>){
+     
     
-      print "enter file name\n";
-      my $name = <STDIN>;
-      chomp $name;
-      ad_fields($name); #call Sub
-        
-        
-        
-        
-        
-        
+    if ($line =~  m/.*?(itemprop="title">.*?h1>).*/s){ #find title
+       my @string = split /[<>]/, $line;
+        my $title = $string[2];
+        $results[0] = $title;
+         
+    }
+   
+    
+    if ($line =~  m/.*?(<span itemprop="jobLocation">.*?<br).*/s){ #find location
+        my @string = split /[<>]/, $line;
+        my $location =$string[12] .' '. $string[14];
       
+        $results[1]=$location;
+    }
+    
+    
+    if ($line =~  m/.*?("Industry".*?<br).*/s){  #find sector
+        my @string = split /[<>]/, $line;
+        my $sector =$string[22];
+       $results[2]=$sector;
+    
+    }
+    
+      if ($line =~  m/.*?(<span itemprop="description">.*?span>).*/s){ #find description
+        my @string = split /[<>]/, $line;
+        my $description =$string[2];
+        print $description;
+        $results[3]=$description;
+    }
+    
+     if ($line =~  m/.*?('job.*?-).*/s){    # find id
+        my @string = split /[job-]/, $line;
+        my $id =$string[3];
+        $id = substr ($id,1,7);
+        $results[4] = $id;
+     
+      
+    }
+    
+  
+ 
+    
+    
+    
+    }
+        %result=(       #assign values from array to hash
+            title=>$results[0],
+            location=>$results[1],
+            sector=>$results[2],
+            description=>$results[3],
+            id=>$results[4]
+        );
+        
+      print Dumper \%result;
+      
+	return {};
+}
+
+ad_fields('data/001-ad.html');
+ 
+
 1;
 __END__
--- ParseCSV.pm
+++ ParseCSV.pm
@@ -1,5 +1,5 @@
 package ParseCSV;
-
+use Data::Dumper qw(Dumper);
 use strict;
 
 
@@ -10,19 +10,22 @@ sub sort_by_population
     my %data;
 
 	my @lines = split( /\n/, $csv_data );
+    
 	foreach my $line (@lines)
 	{
 		my ($id,undef,$capital,$population) = split( /,/, $line );
-		next if $id == '1:id' ;
-
+		next if $id eq '1:id' ;
 		$data{ $population } = $capital;
+        
 	}
-	my ($max) = sort {$a <=> $b} keys %data;
-
-	return "$data{ $max } is the most populated capital";
+	my ($max) = sort {$b <=> $a} keys %data;
+  	return "$data{ $max } is the most populated capital";
 }
 
-
+#sort_by_population("1:id,2:country,3:capital,4:population
+ #1,Greece,Athens,3.753
+ #2,France,Paris,2.244
+ #3,Poland,Warsaw,1.711");
 
 1;
 __END__
